ae248c78c2e1cb681bfaaac84ee64e4b9b9f8651
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-23 16:00:59 +0100

    ok legal

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { CreateRevendicationComponent } from './create-revendication/create-revendication.component';
 import { ObservatoireComponent } from './observatoire/observatoire.component';
 import { PresentationComponent } from './presentation/presentation.component';
+import { LegalComponent } from './legal/legal.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'creer-revendication', component: CreateRevendicationComponent },
   { path: 'observatoire', component: ObservatoireComponent },
   { path: 'presentation', component: PresentationComponent },
+  { path: 'mentions-legales', component: LegalComponent },
 ];
 
 @NgModule({
@@ -18,7 +20,8 @@ const routes: Routes = [
     HomeComponent,
     CreateRevendicationComponent,
     ObservatoireComponent,
-    PresentationComponent
+    PresentationComponent,
+    LegalComponent
   ],
   exports: [RouterModule]
 })
