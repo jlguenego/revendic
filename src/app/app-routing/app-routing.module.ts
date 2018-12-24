c773735f09dcb6e6bb2ed2ce211c00cfb5ce3050
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-24 19:30:21 +0100

    ok

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './login/login.component';
 import { FormModule } from '../form/form.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { WidgetModule } from '../widget/widget.module';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -17,6 +18,7 @@ const routes: Routes = [
   { path: 'presentation', component: PresentationComponent },
   { path: 'mentions-legales', component: LegalComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'nouveau-compte', component: CreateAccountComponent },
 ];
 
 @NgModule({
@@ -27,7 +29,8 @@ const routes: Routes = [
     ObservatoireComponent,
     PresentationComponent,
     LegalComponent,
-    LoginComponent
+    LoginComponent,
+    CreateAccountComponent
   ],
   exports: [RouterModule]
 })
