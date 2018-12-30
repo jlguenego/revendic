f5079e8e08a4d1eb1993863b9cf3ffe6f141ddaf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-30 11:51:59 +0100

    moving component to user-routing

--- src/app/libuser/user-routing/user-routing.module.ts
+++ src/app/libuser/user-routing/user-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LoginComponent } from 'src/app/app-routing/login/login.component';
-import { CreateAccountComponent } from 'src/app/app-routing/create-account/create-account.component';
-import { PasswordForgottenComponent } from 'src/app/app-routing/password-forgotten/password-forgotten.component';
-import { AccountCreatedComponent } from 'src/app/app-routing/account-created/account-created.component';
-import { ResetMailSentComponent } from 'src/app/app-routing/reset-mail-sent/reset-mail-sent.component';
-import { ManageAccountComponent } from 'src/app/app-routing/manage-account/manage-account.component';
-import { DeletedAccountComponent } from 'src/app/app-routing/deleted-account/deleted-account.component';
-import { VerifyAccountComponent } from 'src/app/app-routing/verify-account/verify-account.component';
-import { UpdatedAccountComponent } from 'src/app/app-routing/updated-account/updated-account.component';
 import { FormModule } from 'src/app/form/form.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { WidgetModule } from 'src/app/widget/widget.module';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { PasswordForgottenComponent } from './password-forgotten/password-forgotten.component';
+import { AccountCreatedComponent } from './account-created/account-created.component';
+import { ResetMailSentComponent } from './reset-mail-sent/reset-mail-sent.component';
+import { ManageAccountComponent } from './manage-account/manage-account.component';
+import { DeletedAccountComponent } from './deleted-account/deleted-account.component';
+import { VerifyAccountComponent } from './verify-account/verify-account.component';
+import { UpdatedAccountComponent } from './updated-account/updated-account.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
