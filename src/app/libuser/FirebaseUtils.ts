11c5b0a3c68fc479fea4f26b0b1bd9a66da4d217
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-30 18:06:42 +0100

    ok

--- src/app/libuser/FirebaseUtils.ts
+++ src/app/libuser/FirebaseUtils.ts
@@ -2,7 +2,9 @@
 const config = {
     "auth/requires-recent-login": "Pour faire cette opération. Vous avez besoin d'être récemment connecté.",
     "auth/user-token-expired": "Votre session a expiré.",
-    "auth/too-many-requests": "Erreur technique. Site trop surchargé. Réessayer ultérieurement."
+    "auth/too-many-requests": "Erreur technique. Site trop surchargé. Réessayer ultérieurement.",
+    "auth/wrong-password": "Mot de passe invalide.",
+    "auth/weak-password": "Mot de passe trop faible. Il doit contenir plus de 6 caractères.",
 }
 
 export class FirebaseUtils {
