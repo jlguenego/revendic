d23027ff2a9083dff9eb74823782394c417133f9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-31 18:20:22 +0100

    create revendication

--- src/app/rev/create-revendication/create-revendication.component.ts
+++ src/app/rev/create-revendication/create-revendication.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { UserService } from 'src/app/user/user.service';
+
 import { RevService } from '../rev.service';
 
 @Component({
@@ -16,13 +19,14 @@ export class CreateRevendicationComponent implements OnInit {
     content: new FormControl(''),
   });
 
-  constructor() { }
+  constructor(private rev: RevService) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
     console.log('create revendication');
+    this.rev.add(this.f.value.content);
     
   }
 
--- src/app/rev/rev.service.ts
+++ src/app/rev/rev.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@angular/core';
+import { RevendicationRecord } from './revendication.record';
+import { AngularFirestore } from '@angular/fire/firestore';
+
+import { UserService } from '../user/user.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +14,19 @@ export class RevService {
     UNKNOWN: 0,
   }
 
-  constructor() { }
+  constructor(private db: AngularFirestore, private user: UserService, private router: Router) { }
+
+  add(content: string) {
+    const revendicationRecord: RevendicationRecord = {
+      title: content,
+      author: this.user.displayName,
+    };
+
+    this.db.collection("revendications").add(revendicationRecord).then(docRef => {
+      console.log("Document written with ID: ", docRef.id);
+      this.router.navigate(['/']);
+    }).catch(error => {
+      console.error("Error adding document: ", error);
+    });
+  }
 }
