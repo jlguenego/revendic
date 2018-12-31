06947884a803b810c4bf858c50cacee3bc3f63fb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-12-31 18:00:58 +0100

    read data from Firestore

--- src/app/rev/revendication-list/revendication-list.component.ts
+++ src/app/rev/revendication-list/revendication-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RevendicationRecord } from '../revendication.record';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-revendication-list',
@@ -11,23 +13,44 @@ export class RevendicationListComponent implements OnInit {
 
   revendications: RevendicationRecord[];
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.revendications = [
-      {
-        title: 'Je veux un gros SMIC !',
-        author: {
-          displayName: 'Marcel Duschmoll'
-        }
-      },
-      {
-        title: 'Je veux le RIC ou je casse tout !',
-        author: {
-          displayName: 'Simone Lavieille-Charue'
-        }
-      }
-    ]
+    // this.revendications = [
+    //   {
+    //     title: 'Je veux un gros SMIC !',
+    //     author: {
+    //       displayName: 'Marcel Duschmoll'
+    //     }
+    //   },
+    //   {
+    //     title: 'Je veux le RIC ou je casse tout !',
+    //     author: {
+    //       displayName: 'Simone Lavieille-Charue'
+    //     }
+    //   }
+    // ];
+
+    this.db.collection('/revendications').valueChanges()
+      .subscribe(revendications => {
+        this.revendications = [];
+        revendications.forEach(rev => {
+          const revendicationRecord: RevendicationRecord = {
+            title: rev['title'],
+            author: {
+              displayName: ''
+            }
+          }
+          this.revendications.push(revendicationRecord);
+          const reference: DocumentReference = rev['author'];
+          console.log('typeof reference', reference);
+          this.db.collection('/users').doc(reference.id).valueChanges().subscribe(
+            user => revendicationRecord.author.displayName = user['displayName']
+          );
+        })
+      });
+
+
   }
 
 }
