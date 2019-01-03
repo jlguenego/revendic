efe38eb6cd56beb03ba641fb6fc38817e0bb79b0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-03 18:57:39 +0100

    question

--- src/app/rev/create-revendication/create-revendication.component.ts
+++ src/app/rev/create-revendication/create-revendication.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { UserService } from 'src/app/user/user.service';
 
 import { RevService } from '../rev.service';
 import { Router } from '@angular/router';
@@ -22,9 +20,41 @@ export class CreateRevendicationComponent implements OnInit {
     category: new FormControl(''),
   });
 
+  question = new FormGroup({
+    mode: new FormControl(''),
+    category: new FormControl(''),
+    category2: new FormControl(''),
+  });
+
+  showForm = false;
+
+  previous;
+
   constructor(private rev: RevService, private router: Router) { }
 
   ngOnInit() {
+    this.question.valueChanges.subscribe(value => {
+      console.log('previous', this.previous);
+      console.log('value', value);
+
+      if (this.previous === undefined) {
+        this.previous = value;
+      }
+
+      if (value.mode !== this.previous.mode && value.category !== '') {
+        this.question.setValue({ mode: value.mode, category: '', category2: '' });
+      }
+
+      if (value.category !== this.previous.category && value.category2 !== '') {
+        this.question.setValue({ mode: value.mode, category: value.category, category2: '' });
+      }
+
+      const newValue = this.question.value;
+      this.showForm = (newValue.mode === 'expert') ||
+        (newValue.category2 !== '');
+
+      this.previous = value;
+    });
   }
 
   onSubmit() {
