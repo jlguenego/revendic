9ab50cb9cef6e43c58467559fae6ce0c02c50dad
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-07 16:35:00 +0100

    design motion

--- src/app/app-routing/home/home.component.ts
+++ src/app/app-routing/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  faYoutube = faYoutube;
 
   constructor() { }
 
