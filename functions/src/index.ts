330696a1c7b067f14026a4524c6d9c7f7081cd63
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-08 18:03:40 +0100

    ssr help

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 
+// Help for SSR : https://hackernoon.com/deploying-angular-universal-v6-with-firebase-c86381ddd445
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
