3af3c3e62cd4f06e895159fb5d7bdc8b8f3ddfae
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-01-10 16:07:50 +0100

    photo

--- src/app/rev/revendication.record.ts
+++ src/app/rev/revendication.record.ts
@@ -2,11 +2,14 @@ export interface RevendicationRecord {
     id?: string;
     title?: string;
     author?: string;
+    content?: any;
+    photo?: string;
+    category?: any;
+
     createdAt?: any;
     updatedAt?: any;
+
     userid?: any;
-    category?: any;
-    content?: any;
     _random?: any;
     likes?: any;
     dislikes?: any;
