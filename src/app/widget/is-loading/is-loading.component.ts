af2d828fb6bfb01abc7ef9aefae77ff3ced3a149
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-03 14:50:52 +0100

    progressbar while starting

--- src/app/widget/is-loading/is-loading.component.ts
+++ src/app/widget/is-loading/is-loading.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/user/user.service';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-is-loading',
@@ -8,9 +9,19 @@ import { UserService } from 'src/app/user/user.service';
 })
 export class IsLoadingComponent implements OnInit {
 
+  startFadingOut = false;
+  enabled = true;
+
   constructor(public user: UserService) { }
 
   ngOnInit() {
+    const subscription = this.user.subject.subscribe(v => {
+      if (v !== null) {
+        this.startFadingOut = true;
+        timer(1000).subscribe(v => this.enabled = false);
+        subscription.unsubscribe();
+      }
+    });
   }
 
 }
