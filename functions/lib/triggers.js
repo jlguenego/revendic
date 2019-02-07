758c3af747a12f3fd0918d8b9b8a557a2dcd13f2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-02-07 17:23:27 +0100

    trigger

--- functions/lib/triggers.js
+++ functions/lib/triggers.js
@@ -1,15 +1,32 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 const likeTrigger = functions.firestore.document('likes-revendications/{revid}/users/{userid}')
     .onWrite((change, context) => {
-        console.log('change', change);
+        // console.log('change', change);
         console.log('context', context);
 
         // setup 3 fields in the revendication
         // like, dislike, voters
-        
+        const rev = admin.firestore().doc(`/revendications/${context.params.revid}`);
+        const votersRef = admin.firestore().collection(`/likes-revendications/${context.params.revid}/users`);
+        return votersRef.get().then(querySnapshot => {
+            console.log('querySnapshot', querySnapshot.size);
+            const docs = querySnapshot.docs.map(doc => {
+                const id = doc.id;
+                const data = doc.data();
+                return {id, like: data.like};
+            });
+            console.log('docs', docs.length);
+            const likers = docs.filter(doc => +doc.like === 1).map(doc => doc.id);
+            const like = likers.length;
+            console.log('like', like);
+            const dislikers = docs.filter(doc => +doc.like === -1).map(doc => doc.id);
+            const dislike = dislikers.length;
+            console.log('dislike', dislike);
+            return rev.update({ like, dislike, likers, dislikers });
+        });
 
-        return 0;
     });
 
 module.exports = {
